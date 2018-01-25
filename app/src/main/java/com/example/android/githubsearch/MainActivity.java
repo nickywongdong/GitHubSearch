9176973de9ca62cca7b200c8e577985c4dc51a6b
Rob Hess <robwhess@gmail.com>
2018-01-25 15:41:05 -0800

    Add initial code.

---
+++ app/src/main/java/com/example/android/githubsearch/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.githubsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mSearchResultsRV;
+    private EditText mSearchBoxET;
+    private GitHubSearchAdapter mGitHubSearchAdapter;
+
+    private String[] dummySearchResults = {
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchBoxET = (EditText)findViewById(R.id.et_search_box);
+        mSearchResultsRV = (RecyclerView)findViewById(R.id.rv_search_results);
+
+        mSearchResultsRV.setLayoutManager(new LinearLayoutManager(this));
+        mSearchResultsRV.setHasFixedSize(true);
+
+        mGitHubSearchAdapter = new GitHubSearchAdapter();
+        mSearchResultsRV.setAdapter(mGitHubSearchAdapter);
+
+        Button searchButton = (Button)findViewById(R.id.btn_search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchQuery = mSearchBoxET.getText().toString();
+                if (!TextUtils.isEmpty(searchQuery)) {
+                    mGitHubSearchAdapter.updateSearchResults(new ArrayList<>(Arrays.asList(dummySearchResults)));
+                    mSearchBoxET.setText("");
+                }
+            }
+        });
+    }
+}
