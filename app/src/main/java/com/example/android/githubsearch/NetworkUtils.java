0dc275f8d7b9cc1e5c3d338f4a47175dd4214fdd
wongnich <wongnich@gmail.com>
2018-02-01 14:21:08 -0800

    included permissions in manifest, and working on changing object for network utils

--- app/src/main/java/com/example/android/githubsearch/NetworkUtils.java
+++ app/src/main/java/com/example/android/githubsearch/NetworkUtils.java
@@ -6,13 +6,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+import okhttp3.OkHttpClient;
+
 /**
  * Created by hessro on 1/30/18.
  */
 
 public class NetworkUtils {
-    public static String doHTTPGet(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+    private static final OkHttpClient mHTTPClient = new OkHttpClient();
+
+    public static String doHTTPGet(String url) throws IOException {
+
+        
+        /*HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
 
         InputStream inputStream;
         try {
@@ -28,6 +35,6 @@ public class NetworkUtils {
             return scanner.next();
         } else {
             return null;
-        }
+        }*/
     }
 }
